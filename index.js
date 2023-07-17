7c69fd7e25aedff4b691d34a79f41f01a8238920
Alessandro1918 <alessandro1918@hotmail.com>
2023-07-17 17:38:17 -0300

    feat: basic connection structure with socket.io package

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const cors = require("cors")
 const express = require("express")
 require('dotenv').config()
 
@@ -6,13 +5,29 @@ const PORT = process.env.PORT || 4000
 const URL_FRONT = process.env.URL_FRONT || `http://localhost:3000`
 
 const app = express()
-
-app.use(cors({
-  origin: URL_FRONT
-}))
-
+// app.use(cors({
+//   origin: URL_FRONT
+// }))
 app.get("/", (req, res) => {
   res.send(`<h1>Hello, World!</h1>`)
 })
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
+const server = require("http").createServer(app)
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: URL_FRONT
+  }
+})
+
+io.on("connection", (socket) => {
+
+  //Logs at every connections, no matter the room:
+  console.log("Client", socket.id, "connected")
+
+  socket.on("disconnecting", () => {
+    console.log("Client", socket.id, "disconnected")
+  })
+})
+
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
